767a44b9c93abce8af025eedc1144602401944dd
adirama11 <jidaneadir11@gmail.com>
2024-08-28 11:11:54 +0700

    Update fitur login

--- program/controllers/userscontrollers/userscontrollers.js
+++ program/controllers/userscontrollers/userscontrollers.js
@@ -47,38 +47,100 @@ const Register = async (req, res) => {
 
   const newPass = await enkripsi.hash(req.body.password, 10);
 
- const fullname = req.body.fullname
+  const fullname = req.body.fullname;
 
-  const tokens = jwt.sign(
-    { fullname },
-    process.env.ACCES_KEY,
-    {
-      expiresIn: "360s",
-    }
-  );
+  const tokens = jwt.sign({ fullname }, process.env.ACCES_KEY, {
+    expiresIn: "360s",
+  });
 
   const data = {
     name: req.body.fullname,
     email: req.body.email,
     password: newPass,
-    remember_token: tokens
+    remember_token: tokens,
   };
   const createUsers = await Users.create(data);
 
   return res.status(200).json({
     status: true,
-    message:"Sukses register account",
-    result:{
-      token:tokens,
+    message: "Sukses register account",
+    result: {
+      token: tokens,
       user: {
-        id:createUsers.id,
+        id: createUsers.id,
         name: createUsers.name,
-        email : createUsers.email,
-        role:"user",
-        register_at:createUsers.createdAt,
-      }
-    }
-  })
+        email: createUsers.email,
+        role: "user",
+        register_at: createUsers.createdAt,
+      },
+    },
+  });
 };
 
-module.exports = { Register };
+const Login = async (req, res) => {
+  const schema = {
+    email: "string | empty:false",
+    password: "string | empty:false | min : 8",
+  };
+
+  const validation = v.validate(req.body, schema);
+
+  if (validation.lenght) {
+    return res.status(400).json({
+      msg: validation,
+    });
+  }
+
+  const cek = await Users.findOne({
+    where: { email: req.body.email },
+  });
+
+  if (!cek) {
+    return res.status(404).json({
+      status: "error",
+      message: "User not found",
+    });
+  }
+
+  if(cek.type=1){
+    cek.type="admin"
+  }else{
+    cek.type="user"
+  }
+
+  const invalidPassword = await enkripsi.compare(req.body.password, cek.password);
+
+    if (!invalidPassword){
+        return res.status(200).json({
+            status: 'error',
+            message: 'User not foundd'
+        });
+    }
+
+
+  const id = cek.id;
+  const name = cek.name;
+  const role = cek.type;
+  const email = cek.email;
+  const regis = cek.createdAt
+
+  const token = jwt.sign({ name }, process.env.ACCES_KEY, {
+    expiresIn: "360s",
+  });
+
+  return res.status(200).json({
+    status: "succes",
+    message: "Login sukses!",
+    result: {
+      token: token,
+      user: {
+        id: id,
+        name: name,
+        email:email,
+        role: role,
+        register_at:regis
+      },
+    },
+  });
+};
+module.exports = { Register, Login };
--- program/routes/users.js
+++ program/routes/users.js
@@ -2,5 +2,5 @@ const express = require('express');
 const router = express.Router();
 const usersControllers = require('../controllers/userscontrollers/userscontrollers')
 router.post("/", usersControllers.Register);
-
+router.post("/auth", usersControllers.Login);
 module.exports = router;

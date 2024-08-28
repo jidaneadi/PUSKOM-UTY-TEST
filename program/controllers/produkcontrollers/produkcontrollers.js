b9bc30cd5f0526b5b632bd85d02b1b31fcd0e80d
adirama11 <jidaneadir11@gmail.com>
2024-08-28 10:58:59 +0700

    Update fitur get Produk [Finish]

--- program/controllers/produkcontrollers/produkcontrollers.js
+++ program/controllers/produkcontrollers/produkcontrollers.js
@@ -1,12 +1,21 @@
-const { Products } = require('../../models')
+const { QueryTypes } = require('sequelize')
+const { Products, sequelize } = require('../../models')
 
 const ShowProduct = async(req, res) => {
     
-    const dataProducts = await Products.findAll({
-        attributes: ['product_id', 'product_name', 'product_brand', 'product_price', 'product_stock']
-    });
+    const dataProducts = await sequelize.query('SELECT products.product_id, products.product_name, products.product_brand, products.product_price, products.product_stock, brands.brand_id, brands.brand_name FROM products LEFT JOIN brands ON brands.brand_id = products.product_id;',
+    {
+        type: QueryTypes.SELECT
+      })
 
-    return  res.status(200).json(dataProducts)
+    if(dataProducts === " "){
+        return res.status(404).json({msg : "Data transaksi kosong!"})
+    }
+    return  res.status(200).json({
+        status: true,
+        message: "Sukses get produk",
+        result: dataProducts
+    })
 }
 
 module.exports= { ShowProduct };
